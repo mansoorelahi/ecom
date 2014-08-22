3e6a7f3335e6b67b970f9dfc273cefee4db5c3c5
Prakash Natarajan <prakash.n@smnetserv.com>
2014-08-22 17:16:23 +0530

    database changes for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   #devise :omniauthable, :omniauth_providers => [:facebook]
+
+
+  ##callbacks
+
+  before_save do |user|
+    if user.first_name? and user.last_name.blank?
+      user.first_name = user.email.split("@")[0]
+    end
+  end
+
+  def full_name
+    "#{self.first_name unless self.first_name.blank?} #{self.last_name unless self.last_name.blank?}"
+  end
 end
